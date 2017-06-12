bb834729be5c26cbec80e62ca9cdc1e231f99561
Sebastian Fox <sebastian.fox@phe.gov.uk>
2017-06-12 10:45:52 +0100

    testthat updated to shorter test

--- tests/testthat/test-extract.R
+++ tests/testthat/test-extract.R
@@ -3,9 +3,9 @@ library(fingertipsR)
 
 context("fingertips data extract")
 
-df1 <- fingertips_data(IndicatorID = 1107)
-df2 <- suppressWarnings(fingertips_data(IndicatorID = 1107, DomainID = 1938133055))
-df3 <- suppressWarnings(fingertips_data(IndicatorID = 1107, ProfileID = 8))
+df1 <- fingertips_data(IndicatorID = 92309)
+df2 <- suppressWarnings(fingertips_data(IndicatorID = 92309, DomainID = 1938133055))
+df3 <- suppressWarnings(fingertips_data(IndicatorID = 92309, ProfileID = 8))
 
 test_that("the data returned are the same despite different inputs", {
         expect_equal(df1, df2)
@@ -14,18 +14,18 @@ test_that("the data returned are the same despite different inputs", {
 })
 
 test_that("warning messages work", {
-        expect_warning(fingertips_data(IndicatorID = 1107, DomainID = 1938133055),
+        expect_warning(fingertips_data(IndicatorID = 92309, DomainID = 1938133055),
                        "IndicatorID is complete so DomainID and/or ProfileID inputs are ignored")
         expect_warning(fingertips_data(DomainID = 1938133055, ProfileID = 8),
                        "DomainID is complete so ProfileID is ignored")
-        expect_warning(fingertips_data(IndicatorID = 1107, AreaTypeID = 102, ParentAreaTypeID = 101),
+        expect_warning(fingertips_data(IndicatorID = 92309, AreaTypeID = 102, ParentAreaTypeID = 101),
                        "AreaTypeID not a child of ParentAreaTypeID\\. There may be duplicate values in data\\. Use function area_types\\(\\) to see mappings of area type to parent area type.")
 })
 
 test_that("error messages work", {
         expect_error(fingertips_data(), "One of IndicatorID, DomainID or ProfileID must have an input")
-        expect_error(fingertips_data(IndicatorID = 1107, AreaTypeID = "hello"),
+        expect_error(fingertips_data(IndicatorID = 92309, AreaTypeID = "hello"),
                      "Invalid AreaTypeID\\. Use function area_types\\(\\) to see what values can be used\\.")
-        expect_error(fingertips_data(IndicatorID = 1107, AreaTypeID = NULL),
+        expect_error(fingertips_data(IndicatorID = 92309, AreaTypeID = NULL),
                      "AreaTypeID must have a value\\. Use function area_types\\(\\) to see what values can be used\\.")
 })
