1df2a25440a8da6169cb40d20f1693d73c1d49fd
Sebastian Fox <sebastian.fox@phe.gov.uk>
2017-08-01 17:01:24 +0100

    draft paper edits

---
+++ vignettes/fingertips_paper_captions.R
@@ -0,0 +1,5 @@
+# fingertips_paper_captions.R
+
+fig_caps <- captioner(prefix = "Figure")
+
+fig_caps(name = "life_exp_birth", "Life expectancy at birth by Index of Multiple Deprivation score, Upper Tier Local Authority, England 2012-14")
