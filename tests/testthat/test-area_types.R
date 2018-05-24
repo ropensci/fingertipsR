fc84ab28f005a0f9db86ee2b649fd6d125d065a3
Sebastian Fox <sebastian.fox@phe.gov.uk>
2018-05-24 15:52:02 +0100

    attempts to achieve 100% coverage

--- tests/testthat/test-area_types.R
+++ tests/testthat/test-area_types.R
@@ -29,7 +29,9 @@ test_that("category_types returns as expected", {
 
 context("nearest_neighbours works correctly")
 test_that("nearest_neighbours returns as expected", {
+        expect_is(nearest_neighbours(AreaCode = "E10000007", AreaTypeID = 101), "character")
         expect_is(nearest_neighbours(AreaCode = "E10000007", AreaTypeID = 102, measure = "CIPFA"), "character")
+        expect_is(nearest_neighbours(AreaCode = "E10000007", AreaTypeID = 102, measure = "CSSN"), "character")
         expect_is(nearest_neighbours(AreaCode = "E38000022", AreaTypeID = 153), "character")
         expect_error(nearest_neighbours(AreaCode = "E12000001", AreaTypeID = 6), "AreaTypeID must be one of 101, 102, 152 or 153")
         expect_error(nearest_neighbours(AreaCode = "E07000033", AreaTypeID = 152), "E07000033 not in AreaTypeID = 152")
