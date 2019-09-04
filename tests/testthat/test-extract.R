4c93df19035ddfe9afe852ceb20672434670156b
Sebastian Fox <sebastian.fox@phe.gov.uk>
2019-09-04 15:47:23 +0100

    skip_if_offline() added to all tests to follow CRAN policy

--- tests/testthat/test-extract.R
+++ tests/testthat/test-extract.R
@@ -3,22 +3,35 @@ library(fingertipsR)
 
 context("fingertips data extract")
 
+skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
 df1 <- fingertips_data(IndicatorID = 90616, AreaTypeID = 152)
+skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
 df2 <- suppressWarnings(fingertips_data(IndicatorID = 90616, AreaTypeID = 152, DomainID = 1938133106))
+skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
 df3 <- suppressWarnings(fingertips_data(IndicatorID = 90616, AreaTypeID = 152, ProfileID = 135))
+skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
 df4 <- fingertips_data(DomainID = 1938132767)
+skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
 df5 <- fingertips_data(ProfileID = 152)
+skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
 df6 <- fingertips_data(IndicatorID = 90616, AreaTypeID = 152, ProfileID = NA)
+skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
 df7 <- fingertips_data(DomainID = 1938132767, AreaCode = "E06000015")
+skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
 df8 <- fingertips_data(DomainID = 1938132767, rank = TRUE)
 
 inds <- c(93081, 93275, 93094)
+skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
 df9 <- fingertips_data(inds, ProfileID = 143, AreaTypeID = 3)
 
 # Testing different versions of AreaTypeID = "All"
+skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
 df10 <- fingertips_data(IndicatorID = 10101, AreaTypeID = "All")
+skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
 df11 <- fingertips_data(DomainID = 1938132954, AreaTypeID = "All")
+skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
 df12 <- fingertips_data(ProfileID = 152, AreaTypeID = "All")
+skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
 df13 <- fingertips_data(IndicatorID = 10101, ProfileID = 19, AreaTypeID = "All")
 
 ncols <- 26
@@ -29,32 +42,44 @@ cols_to_check <- c('IndicatorID', 'ParentCode', 'AreaCode',
                    'LowerCI99.8limit', 'UpperCI99.8limit',
                    'Count', 'Denominator')
 test_that("the data returned are the same despite different inputs", {
+        skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
         expect_equal(df1[,cols_to_check], df2[,cols_to_check])
+        skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
         expect_equal(df1[,cols_to_check], df3[,cols_to_check])
 })
 
 test_that("error messages work", {
+        skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
         expect_error(fingertips_data(), "One of IndicatorID, DomainID or ProfileID must have an input")
+        skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
         expect_error(fingertips_data(IndicatorID = 92309, AreaTypeID = "hello"),
                      "Invalid AreaTypeID\\. Use function area_types\\(\\) to see what values can be used\\.")
+        skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
         expect_error(fingertips_data(IndicatorID = 92309, AreaTypeID = NULL),
                      "AreaTypeID must have a value\\. Use function area_types\\(\\) to see what values can be used\\.")
+        skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
         expect_error(fingertips_data(DomainID = 1938132767, AreaCode = "hello"),
                      "Area code not contained in AreaTypeID\\.")
+        skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
         expect_error(fingertips_data(IndicatorID = 90631, ProfileID = c(19, 20)),
                      "If ProfileID and IndicatorID are populated, they must be the same length")
+        skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
         expect_error(fingertips_data(IndicatorID = 90631, categorytype = "testerror"),
                      "categorytype input must be TRUE or FALSE")
+        skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
         expect_error(fingertips_data(),
                      "One of IndicatorID, DomainID or ProfileID must have an input")
 
 })
 
 test_that("warning messages work", {
+        skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
         expect_warning(fingertips_data(DomainID = 1938132767, AreaCode = "E06000015", ParentAreaTypeID = 153),
                        "AreaTypeID not a child of ParentAreaTypeID\\. There may be duplicate values in data\\. Use function area_types\\(\\) to see mappings of area type to parent area type\\.")
+        skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
         expect_warning(fingertips_data(DomainID = 1938133152, ProfileID = 76),
                        "DomainID is complete so ProfileID is ignored")
+        skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
         expect_warning(fingertips_data(IndicatorID = c(93105, 93107),
                                        ProfileID = c(NA, 143),
                                        AreaTypeID = 8),
@@ -62,15 +87,26 @@ test_that("warning messages work", {
 })
 
 test_that(paste("number of fields returned by fingertips_data function are", ncols), {
+        skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
         expect_equal(ncol(df1), ncols)
+        skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
         expect_equal(ncol(df4), ncols)
+        skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
         expect_equal(ncol(df5), ncols)
+        skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
         expect_equal(ncol(df6), ncols)
+        skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
         expect_equal(ncol(df7), ncols)
+        skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
         expect_equal(ncol(df8), ncols + 3)
+        skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
         expect_equal(ncol(df9), ncols)
+        skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
         expect_equal(ncol(df10), ncols)
+        skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
         expect_equal(ncol(df11), ncols)
+        skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
         expect_equal(ncol(df12), ncols)
+        skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
         expect_equal(ncol(df13), ncols)
 })
--- tests/testthat/test-metadata.R
+++ tests/testthat/test-metadata.R
@@ -4,23 +4,33 @@ library(fingertipsR)
 context("Test metadata")
 ncols <- 30
 
+skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
 df1 <- suppressWarnings(indicator_metadata(IndicatorID = 10101))
+skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
 df2 <- suppressWarnings(indicator_metadata(DomainID = 1938132767))
+skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
 df3 <- suppressWarnings(indicator_metadata(ProfileID = 132))
 
 test_that("bad inputs give bad outputs", {
+        skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
         expect_error(indicator_metadata("blah blah"),
                      "IndicatorID\\(s\\) do not exist, use indicators\\(\\) to identify existing indicators")
+        skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
         expect_error(indicator_metadata(DomainID = 1),
                      "DomainID\\(s\\) do not exist, use profiles\\(\\) to identify existing domains")
+        skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
         expect_error(indicator_metadata(ProfileID = "1234 blah blah"),
                      "ProfileID\\(s\\) do not exist, use profiles\\(\\) to identify existing profiles")
+        skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
         expect_error(indicator_metadata(),
                      "One of IndicatorID, DomainID or ProfileID must be populated")
 })
 
 test_that(paste("the number of fields in the output are", ncols), {
+        skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
         expect_equal(ncol(df1), ncols)
+        skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
         expect_equal(ncol(df2), ncols)
+        skip_if_offline(host = "https://fingertips.phe.org.uk/api/")
         expect_equal(ncol(df3), ncols)
 })
