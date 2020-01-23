c0bebf787f291c0583b58aed4217e345fa4d7736
Sebastian Fox <sebastian.fox@phe.gov.uk>
2020-01-23 17:34:57 +0000

    reduced extract and deprivation tests

--- tests/testthat/test-deprivation.R
+++ tests/testthat/test-deprivation.R
@@ -3,68 +3,43 @@ library(fingertipsR)
 
 context("Deprivation extract")
 
-# create progress bar so travis gets responses
-pb <- txtProgressBar(style = 3)
-setTxtProgressBar(pb, 0)
-
-skip_if_offline()
-dep_3_2015 <- deprivation_decile(AreaTypeID = 3, Year = 2015) #
-
-setTxtProgressBar(pb, 0.2)
-skip_if_offline()
-dep_202_2019 <- deprivation_decile(AreaTypeID = 202, Year = 2019) #
-
-setTxtProgressBar(pb, 0.4)
-skip_if_offline()
-dep_201_2019 <- deprivation_decile(AreaTypeID = 201, Year = 2019) #
-
-setTxtProgressBar(pb, 0.6)
-skip_if_offline()
-dep_154_2019 <- deprivation_decile(AreaTypeID = 154, Year = 2019) #
-
-setTxtProgressBar(pb, 0.8)
-skip_if_offline()
-dep_7_2019 <- deprivation_decile(AreaTypeID = 7, Year = 2019) #
-setTxtProgressBar(pb, 1)
-
 dep_cols <- 3
 test_that("the number of columns of deprivation decile function are as expected", {
         skip_if_offline()
-        expect_equal(ncol(dep_3_2015), dep_cols)
-        skip_if_offline()
-        expect_equal(ncol(dep_154_2019), dep_cols)
+        expect_equal(ncol(deprivation_decile(AreaTypeID = 202, Year = 2015)), dep_cols)
+
 })
 
 test_that("the number of rows returned by deprivation decile is greater than 2", {
         skip_if_offline()
-        expect_true(nrow(dep_201_2019) > 2)
-        skip_if_offline()
-        expect_true(nrow(dep_202_2019) > 2)
+        expect_true(nrow(deprivation_decile(AreaTypeID = 201, Year = 2019)) > 2)
+
+        expect_true(nrow(deprivation_decile(AreaTypeID = 202, Year = 2019)) > 2)
 })
 
 exp_classes <- c("character","numeric","integer")
 names(exp_classes) <- c("AreaCode", "IMDscore", "decile")
 test_that("the class of columns returned are character-numeric-integer", {
         skip_if_offline()
-        expect_equal(vapply(dep_7_2019, class, character(1)), exp_classes)
+        expect_equal(vapply(deprivation_decile(AreaTypeID = 202, Year = 2019), class, character(1)), exp_classes)
 })
 
 test_that("error messages work correctly", {
         skip_if_offline()
         expect_error(deprivation_decile(Year = 2014, AreaTypeID = 102), "Year must be 2015 or 2019")
-        skip_if_offline()
+
         expect_error(deprivation_decile(AreaTypeID = 12, Year = 2019),
                      "AreaTypeID not available")
-        skip_if_offline()
+
         expect_error(deprivation_decile(AreaTypeID = 3, Year = 2019),
                      "AreaTypeID unavailable for 2019")
-        skip_if_offline()
+
         expect_error(deprivation_decile(Year = 2015),
                      "AreaTypeID must be specified")
 })
 
 test_that("warnings work correctly", {
-        skip_if_offline()
+
         expect_warning(deprivation_decile(AreaTypeID = 120, Year = 2015),
                      "All the values are NA in this data; this can happen when the data are automatically calculated")
 
--- tests/testthat/test-extract.R
+++ tests/testthat/test-extract.R
@@ -3,75 +3,30 @@ library(fingertipsR)
 
 context("fingertips data extract")
 
-skip_if_offline()
-df1 <- fingertips_data(IndicatorID = 90616, AreaTypeID = 120)
-skip_if_offline()
-df2 <- suppressWarnings(fingertips_data(IndicatorID = 90616, AreaTypeID = 120, DomainID = 1938133106))
-skip_if_offline()
-df3 <- suppressWarnings(fingertips_data(IndicatorID = 90616, AreaTypeID = 120, ProfileID = 135))
-skip_if_offline()
-df4 <- fingertips_data(DomainID = 1938133301, AreaTypeID = 6)
-skip_if_offline()
-df5 <- fingertips_data(ProfileID = 156, AreaTypeID = 6)
-skip_if_offline()
-df6 <- suppressWarnings(fingertips_data(IndicatorID = 90616, AreaTypeID = 120, ProfileID = NA))
-skip_if_offline()
-df7 <- fingertips_data(DomainID = 1938133301, AreaCode = "E12000005", AreaTypeID = 6)
-skip_if_offline()
-df8 <- fingertips_data(DomainID = 1938133301, AreaTypeID = 6, rank = TRUE)
-
-inds <- c(93081, 93275)
-skip_if_offline()
-df9 <- fingertips_data(inds, ProfileID = 143, AreaTypeID = 3)
-
-# Testing different versions of AreaTypeID = "All"
-skip_if_offline()
-df10 <- fingertips_data(IndicatorID = 10101, AreaTypeID = "All", path = 'https://fingertips.phe.org.uk/api/')
-skip_if_offline()
-df11 <- fingertips_data(DomainID = 1938133301, AreaTypeID = "All")
-skip_if_offline()
-df12 <- fingertips_data(ProfileID = 156, AreaTypeID = "All")
-skip_if_offline()
-df13 <- fingertips_data(IndicatorID = 90362, ProfileID = 156, AreaTypeID = "All")
-skip_if_offline()
-df14 <- suppressWarnings(fingertips_data(IndicatorID = 90362, AreaTypeID = 6, ProfileID = 156, rank = TRUE))
-
 ncols <- 26
-
-cols_to_check <- c('IndicatorID', 'ParentCode', 'AreaCode',
-                   'Sex', 'Age', 'Timeperiod', 'Value',
-                   'LowerCI95.0limit', 'UpperCI95.0limit',
-                   'LowerCI99.8limit', 'UpperCI99.8limit',
-                   'Count', 'Denominator')
-test_that("the data returned are the same despite different inputs", {
-        skip_if_offline()
-        expect_equal(df1[,cols_to_check], df2[,cols_to_check])
-        skip_if_offline()
-        expect_equal(df1[,cols_to_check], df3[,cols_to_check])
-})
 
 test_that("error messages work for fingertips_data", {
         skip_if_offline()
         expect_error(fingertips_data(), "One of IndicatorID, DomainID or ProfileID must have an input")
-        skip_if_offline()
+
         expect_error(fingertips_data(IndicatorID = 92309, AreaTypeID = "hello"),
                      "Invalid AreaTypeID\\. Use function area_types\\(\\) to see what values can be used\\.")
-        skip_if_offline()
+
         expect_error(fingertips_data(IndicatorID = 92309, AreaTypeID = NULL),
                      "AreaTypeID must have a value\\. Use function area_types\\(\\) to see what values can be used\\.")
-        skip_if_offline()
+
         expect_error(fingertips_data(DomainID = 1938132767, AreaTypeID = 202, AreaCode = "hello"),
                      "Area code not contained in AreaTypeID\\.")
-        skip_if_offline()
+
         expect_error(fingertips_data(IndicatorID = 90631, AreaTypeID = 202, ProfileID = c(19, 20)),
                      "If ProfileID and IndicatorID are populated, they must be the same length")
-        skip_if_offline()
+
         expect_error(fingertips_data(IndicatorID = 90631, AreaTypeID = 202, categorytype = "testerror"),
                      "categorytype input must be TRUE or FALSE")
-        skip_if_offline()
+
         expect_error(fingertips_data(),
                      "One of IndicatorID, DomainID or ProfileID must have an input")
-        skip_if_offline()
+
         expect_error(fingertips_data(IndicatorID = 90631),
                      "AreaTypeID must be defined")
 
@@ -81,10 +36,13 @@ test_that("warning messages work for fingertips_data", {
         skip_if_offline()
         expect_warning(fingertips_data(DomainID = 1938133293, AreaTypeID = 202, AreaCode = "E06000015", ParentAreaTypeID = 153),
                        "AreaTypeID not a child of ParentAreaTypeID\\. There may be duplicate values in data\\. Use function area_types\\(\\) to see mappings of area type to parent area type\\.")
-        skip_if_offline()
+
         expect_warning(fingertips_data(DomainID = 1938133301, ProfileID = 156, AreaTypeID = 6),
                        "DomainID is complete so ProfileID is ignored")
-        skip_if_offline()
+
+        expect_warning(fingertips_data(IndicatorID = 90366, DomainID = 1938133301, AreaTypeID = 6),
+                       "If IndicatorID is populated DomainID is ignored")
+
         expect_warning(fingertips_data(IndicatorID = c(93105, 93107),
                                        ProfileID = c(NA, 143),
                                        AreaTypeID = 8),
@@ -93,27 +51,30 @@ test_that("warning messages work for fingertips_data", {
 
 test_that(paste("number of fields returned by fingertips_data function are", ncols), {
         skip_if_offline()
-        expect_equal(ncol(df1), ncols)
-        skip_if_offline()
-        expect_equal(ncol(df4), ncols)
-        skip_if_offline()
-        expect_equal(ncol(df5), ncols)
-        skip_if_offline()
-        expect_equal(ncol(df6), ncols)
-        skip_if_offline()
-        expect_equal(ncol(df7), ncols)
-        skip_if_offline()
-        expect_equal(ncol(df8), ncols + 3)
-        skip_if_offline()
-        expect_equal(ncol(df9), ncols)
-        skip_if_offline()
-        expect_equal(ncol(df10), ncols)
-        skip_if_offline()
-        expect_equal(ncol(df11), ncols)
-        skip_if_offline()
-        expect_equal(ncol(df12), ncols)
-        skip_if_offline()
-        expect_equal(ncol(df13), ncols)
-        skip_if_offline()
-        expect_equal(ncol(df14), ncols + 3)
+        expect_equal(ncol(fingertips_data(IndicatorID = 90616, AreaTypeID = 120)), ncols)
+
+        expect_equal(ncol(fingertips_data(DomainID = 1938133301, AreaTypeID = 6)), ncols)
+
+        expect_equal(ncol(fingertips_data(ProfileID = 156, AreaTypeID = 6)), ncols)
+
+        expect_equal(ncol(suppressWarnings(fingertips_data(IndicatorID = 90616,
+                                                           AreaTypeID = 120,
+                                                           ProfileID = NA))), ncols)
+
+        expect_equal(ncol(fingertips_data(DomainID = 1938133301, AreaCode = "E12000005", AreaTypeID = 6)), ncols)
+
+        expect_equal(ncol(fingertips_data(DomainID = 1938133301, AreaTypeID = 6, rank = TRUE)), ncols + 3)
+
+        inds <- c(93081, 93275)
+        expect_equal(ncol(fingertips_data(inds, ProfileID = 143, AreaTypeID = 3)), ncols)
+
+        expect_equal(ncol(fingertips_data(IndicatorID = 10101, AreaTypeID = "All", path = 'https://fingertips.phe.org.uk/api/')), ncols)
+
+        expect_equal(ncol(fingertips_data(DomainID = 1938133301, AreaTypeID = "All")), ncols)
+
+        expect_equal(ncol(fingertips_data(ProfileID = 156, AreaTypeID = "All")), ncols)
+
+        expect_equal(ncol(fingertips_data(IndicatorID = 90362, ProfileID = 156, AreaTypeID = "All")), ncols)
+
+        expect_equal(ncol(suppressWarnings(fingertips_data(IndicatorID = 90362, AreaTypeID = 6, ProfileID = 156, rank = TRUE))), ncols + 3)
 })
