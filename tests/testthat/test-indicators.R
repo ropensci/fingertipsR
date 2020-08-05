dfdebde3a7cb96bf36e91f3ef235e11a5f56e557
Sebastian Fox <sebastian.fox@phe.gov.uk>
2020-08-05 10:54:58 +0100

    testing schedule of cron job

--- tests/testthat/test-indicators.R
+++ tests/testthat/test-indicators.R
@@ -4,56 +4,50 @@ library(fingertipsR)
 context("Profiles and indicators lookup")
 
 test_that("profile warnings work", {
-        skip_if_offline()
-        expect_error(profiles(c(12,14)),
+        skip_on_cran()
+        expect_error(profiles(c(12, 14)),
                        "ProfileID\\(s\\) are not in the list of profile IDs\\. Re-run the function without any inputs to see all possible IDs\\.")
-        skip_if_offline()
         expect_error(profiles(ProfileName = "PHOF"),
                        "Profile names are not in the list of profile names\\. Re-run the function without any inputs to see all possible names\\.")
 
 })
 
 test_that("the profiles function works", {
-        skip_if_offline()
+        skip_on_cran()
         expect_is(profiles(ProfileName = "Public Health Outcomes Framework"),
                        "data.frame")
-        skip_if_offline()
         expect_is(profiles(ProfileID = 19),
                   "data.frame")
-        skip_if_offline()
         expect_is(profiles(ProfileID = c(19, 8)),
                   "data.frame")
 })
 
 test_that("profile indicators work", {
-        skip_if_offline()
+        skip_on_cran()
         expect_warning(indicators(ProfileID = 156, DomainID = 1938133294),
                        "DomainID is ignored as ProfileID has also been entered")
 
 })
 
 test_that("the indicators function works", {
-        skip_if_offline()
+        skip_on_cran()
         expect_is(indicators(19),
                   "data.frame")
-        skip_if_offline()
         expect_is(indicators(DomainID = 1938133294),
                   "data.frame")
 })
 
 test_that("the indicators_unique function works", {
-        skip_if_offline()
+        skip_on_cran()
         expect_is(indicators_unique(19),
                   "data.frame")
 })
 
 test_that("the indicator_order function works", {
-        skip_if_offline()
+        skip_on_cran()
         expect_is(indicator_order(DomainID = 1938133301, AreaTypeID = 6, ParentAreaTypeID = 15),
                   "data.frame")
-        skip_if_offline()
         expect_error(indicator_order(), "All of DomainID, AreaTypeID and ParentAreaTypeID are required")
-        skip_if_offline()
         expect_error(indicator_order(DomainID = 100, AreaTypeID = 102, ParentAreaTypeID = 6),
                      "DomainID does not exist")
 })
--- tests/testthat/test-metadata.R
+++ tests/testthat/test-metadata.R
@@ -14,16 +14,13 @@ skip_if_offline()
 df4 <- indicator_metadata(IndicatorID = 90362, ProfileID = 156)
 
 test_that("bad inputs give bad outputs", {
-        skip_if_offline()
+        skip_on_cran()
         expect_error(indicator_metadata("blah blah"),
                      "IndicatorID\\(s\\) do not exist, use indicators\\(\\) to identify existing indicators")
-        skip_if_offline()
         expect_error(indicator_metadata(DomainID = 1),
                      "DomainID\\(s\\) do not exist, use profiles\\(\\) to identify existing domains")
-        skip_if_offline()
         expect_error(indicator_metadata(ProfileID = "1234 blah blah"),
                      "ProfileID\\(s\\) do not exist, use profiles\\(\\) to identify existing profiles")
-        skip_if_offline()
         expect_error(indicator_metadata(),
                      "One of IndicatorID, DomainID or ProfileID must be populated")
 })
@@ -31,10 +28,7 @@ test_that("bad inputs give bad outputs", {
 test_that(paste("the number of fields in the output are", ncols), {
         skip_if_offline()
         expect_equal(ncol(df1), ncols)
-        skip_if_offline()
         expect_equal(ncol(df2), ncols)
-        skip_if_offline()
         expect_equal(ncol(df3), ncols)
-        skip_if_offline()
         expect_equal(ncol(df4), ncols)
 })
