94812c4e2bef112231c6d1eeaf123278a5632501
Nanayakkara <Hadley.Nanayakkara@dhsc.gov.uk>
2023-11-09 19:06:05 +0000

    Added proxy settings on startup

---
+++ R/zzz.R
@@ -0,0 +1,20 @@
+.onAttach <- function(libname, pkgname) {
+  assign("fingertips_proxy", "default", envir=as.environment("package:fingertipsR"))
+
+  tryCatch(
+    {
+      fingertips_ensure_api_available(proxy_settings = "default")
+    }, error=function(e) {
+      assign("fingertips_proxy", "none", envir=as.environment("package:fingertipsR"))
+      tryCatch(
+        {
+          fingertips_ensure_api_available(proxy_settings = "none")
+        }, error=function(e) {
+          assign("fingertips_proxy", "default", envir=as.environment("package:fingertipsR"))
+          packageStartupMessage("The API is currently unavailable, you may need to reload the package when the API becomes available.")
+        }
+      )
+    }
+  )
+}
+
